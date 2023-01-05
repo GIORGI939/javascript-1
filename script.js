4ba6d3231ea36de3a5e3282e4629f4452cc6af36
GIORGI939 <118130888+GIORGI939@users.noreply.github.com>
2023-01-05 15:51:12 +0400

    javascript-1 new

--- script.js
+++ script.js
@@ -8,18 +8,33 @@ let user2 = {
     age2 : `21`
 }
 
-let difference = `ანა ლევანზე 7 წლით უფროსია`
-console.log(difference);
-console.log(user1.age1-user2.age2);
+// let difference = `ანა ლევანზე 7 წლით უფროსია`
+// console.log(difference);
+// console.log(user1.age1-user2.age2);
+let difference = user1.age1-user2.age2;
+let resultat = `${user1.age1} ${user2.age2}ზე ${difference}ით უფროსია`
+console.log(resultat);
 
 
 // task2
+// კენტის შემთხვევაში
 const studentsNames = [`John`,`Sam`,`Ann`,`Will`,`Andrew`,`Joseph`,`Lorelai`,`Amelie`,`Den`];
 // let studentebisSaxelebi=studentsNames.length;
-let midlletName=studentsNames[studentsNames.length-5];
-console.log(midlletName);
-let midlletName1=studentsNames[4]
-console.log(midllename1);
+// let midlletName=studentsNames[studentsNames.length-5];
+// console.log(midlletName);
+// let midlletName1=studentsNames[4]
+// console.log(midllename1);
+let middle=studentsNames[(studentsNames.length -1) /2]
+console.log(middle);
+// ლუწის შემთხვევაში
+const studentsNames1 = [`John`,`Sam`,`Ann`,`Will`,`Andrew`,`Joseph`,`Lorelai`,`Amelie`];
+let middle1=studentsNames1[studentsNames1.length/2]
+console.log(middle1);
+// მარჯვენა სახელის დაბრუნება
+const studentsNames2 = [`John`,`Sam`,`Ann`,`Will`,`Andrew`,`Joseph`,`Lorelai`,`Amelie`];
+let middle2 = studentsNames2[(studentsNames2.length -2 ) /2]
+console.log(middle2);
+// მარცხენა სახელის დაბრუნება
 
 // task3
 const student = {
@@ -33,28 +48,24 @@ for(let i = 0; i < student.subject.length; i++) {
     console.log(student.subject[i]);
 }
 const fullName = `${student.firstName} ${student.lastName}`
+student.FulName =fullName
 console.log(fullName);
 const result = `${student.firstName} ${student.lastName} is ${student.age} and his roommate ${student.roommate.fullName}`
 console.log(result);
 
 // task4
 const fruitsNumbers = ["Banana","Orange","Apple","Mango",2,12];
-const i = 0
-while(i < fruitsNumbers.length-2, i++) {
-    console.log(fruitsNumber[i]);
-    // i++;
+let p = 0
+while(typeof fruitsNumbers[p] == `string`){
+console.log(fruitsNumbers[p]);
+p++
 }
-const BREAK_VALUE= fruitsNumbers.length-2;
-while(true){
-if(i == BREAK_VALUE) break;
-console.log(fruitsNumbers[i]);
-i++;
-}
+
 
 
 // task5
 const numbers = [12,23,43,11,9,2,121,90]
-if(numbers > 31 && numb % 2 == 0){
+if(numbers > 31 && numbers % 2 == 0){
     console.log('Element is greater than provided value and is EVEN');
 }
 else{
